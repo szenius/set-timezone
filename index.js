17e907628d3dbc50af88ee08cd5281d05a0a66bb
Sze Ying <ting.szeying@gmail.com>
2020-09-13 14:42:50 +0800

    fix: await exec command

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
 const core = require("@actions/core");
 const execa = require("execa");
 
-try {
-  const timezoneWindows = core.getInput("timezoneWindows");
-  const timezoneLinux = core.getInput("timezoneLinux");
-  const timezoneMacos = core.getInput("timezoneMacos");
+const execCommand = async (file, args) => {
+  await execa(file, args).stdout.pipe(process.stdout);
+};
 
-  const platform = process.platform;
-  console.log(`Configuring for platform ${platform}`);
+const setTimezone = async () => {
+  try {
+    const timezoneWindows = core.getInput("timezoneWindows");
+    const timezoneLinux = core.getInput("timezoneLinux");
+    const timezoneMacos = core.getInput("timezoneMacos");
 
-  switch (platform) {
-    case "linux":
-      execa("sudo", ["timedatectl", "set-timezone", timezoneLinux]).stdout.pipe(
-        process.stdout
-      );
-      break;
-    case "darwin":
-      execa("sudo", ["systemsetup", "-settimezone", timezoneMacos]).stdout.pipe(
-        process.stdout
-      );
-      break;
-    case "win32":
-      execa("tzutil", ["/s", timezoneWindows]).stdout.pipe(process.stdout);
-      break;
-    default:
-      core.setFailed(
-        `Platform ${platform} not supported; Only linux, darwin or win32 are supported now`
-      );
+    const platform = process.platform;
+    console.log(`Configuring for platform ${platform}`);
+
+    switch (platform) {
+      case "linux":
+        await execCommand("sudo", [
+          "timedatectl",
+          "set-timezone",
+          timezoneLinux,
+        ]);
+        break;
+      case "darwin":
+        await execCommand("sudo", [
+          "systemsetup",
+          "-settimezone",
+          timezoneMacos,
+        ]);
+        break;
+      case "win32":
+        await execCommand("tzutil", ["/s", timezoneWindows]);
+        break;
+      default:
+        core.setFailed(
+          `Platform ${platform} not supported; Only linux, darwin or win32 are supported now`
+        );
+    }
+  } catch (error) {
+    core.setFailed(error.message);
   }
-} catch (error) {
-  core.setFailed(error.message);
-}
+};
+
+setTimezone();
